e898099456bc3ae88e3f0d8362859969c0950df3
Dorota Jarecka <djarecka@gmail.com>
2012-09-07 17:10:57 +0200

    working 2d upstream; class shift; fix solver state

--- paper/pyt/test.py
+++ paper/pyt/test.py
@@ -3,22 +3,20 @@ import numpy #TODO: as np?
 
 # -- listing 2.3
 
-class half():
+class Shift():
   # TODO? only if isinstance(slice)?
+  def __init__(self,plus,mnus):
+    self.plus = plus
+    self.mnus = mnus
   def __radd__(self, a):
-    return slice(a.start + 1, a.stop + 1)
+    return slice(a.start + self.plus, a.stop + self.plus)
   def __rsub__(self, a):
-    return a
+    return slice(a.start - self.mnus, a.stop - self.mnus)
 
-class one():
-  # TODO? only if isinstance(slice)?
-  def __radd__(self, a):
-    return slice(a.start + 1, a.stop + 1)
-  def __rsub__(self, a):
-    return slice(a.start - 1, a.stop - 1)
 
-h = half()
-l = one() # TODO: is it possible not to use this hack???
+one = Shift(1,1) 
+hlf = Shift(1,0)
+two = Shift(2,2)
 
 # -- listing 2.6
 
@@ -27,19 +25,23 @@ def f(psi_l, psi_r, C):
 
 # -- listing 2.7
 
-def donorcell_1D(psi, C, i, j):
+def donorcell_1D(psi_arg, C_arg, i, j, d):
+  psi = psi_arg.swapaxes(0,d)
+  C = C_arg.swapaxes(0,d)
   return (
-    f(psi[i,   j], psi[i+l, j], C[i+h, j]) - 
-    f(psi[i-l, j], psi[i,   j], C[i-h, j])
-  )
+    f(psi[i,   j], psi[i+one, j], C[i+hlf, j]) - 
+    f(psi[i-one, j], psi[i,   j], C[i-hlf, j])
+  ).swapaxes(0,d)
 
 # -- listing 2.8
 
 def donorcell_2D(psi, n, C, i, j):
-  psi[n+1][i,j] = (psi[n][i,j]
-    - donorcell_1D(psi[n][:], C[0][:], i, j)
-    - donorcell_1D(psi[n][:], C[1][:], i, j)
-  )
+  print "psi,c shape", psi[n].shape, C[0].shape 
+  print "psi.swap, c.swap shape", psi[n].swapaxes(0,1).shape, C[1].swapaxes(0,1).shape
+  psi[n+1][i,j] = (psi[n][i,j] 
+    - donorcell_1D(psi[n], C[0], i, j, 0)
+    - donorcell_1D(psi[n], C[1], j, i, 1)
+  ) # loopa po wymiarach???
 
 # -- listing 2.13
 
@@ -64,9 +66,9 @@ class Solver_2D(object):
 
     hlo = adv.n_halos
     for i in range(2):
-      self.psi.append(numpy.empty((nx + 2 * hlo, ny + 2 * hlo))) 
-    self.C.append(numpy.empty((nx + 1 + 2 * hlo, ny + 2 * hlo)))
-    self.C.append(numpy.empty((nx + 2 * hlo, ny + 1 + 2 * hlo)))
+      self.psi.append(numpy.zeros((nx + 2 * hlo, ny + 2 * hlo))) 
+    self.C.append(numpy.zeros((nx + 1 + 2 * hlo, ny + 2 * hlo)))
+    self.C.append(numpy.zeros((nx + 2 * hlo, ny + 1 + 2 * hlo)))
 
   def solve(self, nt):
     for t in range(nt):
@@ -74,10 +76,11 @@ class Solver_2D(object):
         # TODO: fill halos
         print "t=%d s=%d" % (t, s)
         self.adv.op_2D(self.psi, self.n, self.C, self.i, self.j, s)
-#        self.n  = (self.n + 1) % 2
+        self.n  = (self.n + 1) % 2 - 2
+
 
   def state(self):
-    return self.psi[self.n]
+    return self.psi[self.n][self.i,self.j]
   def Cx(self):
     return self.C[0]
   def Cy(self):
@@ -96,7 +99,7 @@ slv1.Cy()[:] = .5
 
 print slv1.state()
 
-nt = 3
+nt = 1
 
 slv1.solve(nt)
 print slv1.state()
