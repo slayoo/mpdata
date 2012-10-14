d2f1d3d77c6cb5ae2f73d7ce827159969cac1eb7
slayoo <slayoo@igf.fuw.edu.pl>
2012-10-15 01:36:11 +0200

    indentation changes in listings.py

--- paper/pyt/listings.py
+++ paper/pyt/listings.py
@@ -1,6 +1,9 @@
 #listing00
-import numpy 
+# code licensed under the terms of GNU GPL v3
+# copyright holder: University of Warsaw
 #listing01
+import numpy 
+#listing02
 class Shift():
   def __init__(self, plus, mnus):
     self.plus = plus
@@ -15,14 +18,12 @@ class Shift():
       arg.start - self.mnus, 
       arg.stop  - self.mnus
     )
-#listing02
+#listing03
 one = Shift(1,1) 
 hlf = Shift(1,0)
-#listing02
-def pi(d, idx): 
-  return (idx[d], idx[d-1])
-#listing03
+#listing04
 class Solver_2D(object):
+  # ctor
   def __init__(self, adv, bcx, bcy, nx, ny):
     self.adv = adv
     self.n = 0
@@ -36,57 +37,86 @@ class Solver_2D(object):
     self.bcy = bcy(1, self.j, hlo)
 
     self.psi = (
-      numpy.empty((nx + 2 * hlo, ny + 2 * hlo)),
-      numpy.empty((nx + 2 * hlo, ny + 2 * hlo)) 
+      numpy.empty((nx+2*hlo, ny+2*hlo)),
+      numpy.empty((nx+2*hlo, ny+2*hlo)) 
     )
     self.C = (
-      numpy.empty((nx + 1 + 2 * hlo, ny + 2 * hlo)),
-      numpy.empty((nx + 2 * hlo, ny + 1 + 2 * hlo))
+      numpy.empty((nx+1+2*hlo, ny+2*hlo)),
+      numpy.empty((nx+2*hlo,   ny+1+2*hlo))
     )
 
+  # integration logic
   def solve(self, nt):
     for t in range(nt):
       for s in range(self.adv.n_steps):
-        print "t=%d s=%d" % (t, s)
         self.bcx.fill_halos(self.psi[self.n])
         self.bcy.fill_halos(self.psi[self.n])
-        self.adv.op_2D(self.psi, self.n, self.C, self.i, self.j, s)
+        self.adv.op_2D(self.psi, self.n, 
+          self.C, self.i, self.j, s
+        )
         self.n  = (self.n + 1) % 2 - 2
 
+  # accessor methods
   def state(self):
-    return self.psi[self.n][self.i,self.j]
+    return self.psi[self.n][self.i, self.j]
   def Cx(self):
     return self.C[0]
   def Cy(self):
     return self.C[1]
-#listing04
-class Cyclic(object):
-  def __init__(self, d, i, hlo): #TODO: i & j not needed in principle...
-    self.d = d
-    self.left_halo = slice(i.start - hlo, i.start)
-    self.rght_edge = slice(i.stop - hlo, i.stop)
-    self.rght_halo = slice(i.stop, i.stop + hlo)
-    self.left_edge = slice(i.start, i.start + hlo)
-
-  def fill_halos(self, psi):
-    psi[pi(self.d, [self.left_halo, slice(None)])] = psi[pi(self.d, [self.rght_edge, slice(None)])]
-    psi[pi(self.d, [self.rght_halo, slice(None)])] = psi[pi(self.d, [self.left_edge, slice(None)])]
 #listing05
-def f(psi_l, psi_r, C): 
-  return numpy.where(C >= 0, psi_l * C, psi_r * C)
+def pi(d, idx): 
+  return (idx[d], idx[d-1])
 #listing06
-def donorcell_1D(d, psi, C, i, j): 
-  return (
-    f(psi[pi(d, [i,     j])], psi[pi(d, [i+one, j])], C[pi(d, [i+hlf, j])]) - 
-    f(psi[pi(d, [i-one, j])], psi[pi(d, [i,     j])], C[pi(d, [i-hlf, j])]) 
-  )
+class Cyclic(object):
+  # ctor
+  def __init__(self, d, i, hlo): 
+    self.d = d
+    self.left_halo = [
+      slice(i.start-hlo, i.start), slice(None)
+    ]
+    self.rght_edge = [
+      slice(i.stop-hlo, i.stop),   slice(None)
+    ]
+    self.rght_halo = [
+      slice(i.stop, i.stop+hlo),   slice(None)
+    ]
+    self.left_edge = [
+      slice(i.start, i.start+hlo), slice(None)
+    ]
+
+  # method invoked by the solver
+  def fill_halos(self, psi):
+    psi[  pi(self.d, self.left_halo)] = (
+      psi[pi(self.d, self.rght_edge)])
+    psi[  pi(self.d, self.rght_halo)] = (
+      psi[pi(self.d, self.left_edge)])
 #listing07
-def donorcell_2D(psi, n, C, i, j):
-  psi[n+1][i,j] = (psi[n][i,j] 
-    - donorcell_1D(0, psi[n], C[0], i, j)
-    - donorcell_1D(1, psi[n], C[1], j, i)
+def f(psi_l, psi_r, C): 
+  return (
+    .5 * (C + abs(C)) * psi_l + 
+    .5 * (C - abs(C)) * psi_r
   )
 #listing08
+def donorcell(d, psi, C, i, j): 
+  return (
+    f(
+      psi[pi(d, [i,     j])], 
+      psi[pi(d, [i+one, j])], 
+        C[pi(d, [i+hlf, j])]
+    ) - 
+    f(
+      psi[pi(d, [i-one, j])], 
+      psi[pi(d, [i,     j])], 
+        C[pi(d, [i-hlf, j])]
+    ) 
+  )
+#listing09
+def donorcell_2D(psi, n, C, i, j):
+  psi[n+1][i,j] = (psi[n][i,j] 
+    - donorcell(0, psi[n], C[0], i, j)
+    - donorcell(1, psi[n], C[1], j, i)
+  )
+#listing10
 class Mpdata(object):
   def __init__(self, n_iters):
     self.n_steps = n_iters
@@ -95,4 +125,4 @@ class Mpdata(object):
 
   def op_2D(self, psi, n, C, i, j, step):
     donorcell_2D(psi, n, C, i, j)
-#listing09
+#listing11
