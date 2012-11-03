fde9ad1a84a22505b281e930b4ef49a7e6af1238
Dorota Jarecka <djarecka@gmail.com>
2012-11-03 18:34:10 +0100

    new version i python works for mpdata, also for n_iters>2

--- paper/pyt/listings_zmianahalo.py
+++ paper/pyt/listings_zmianahalo.py
@@ -48,17 +48,17 @@ class Solver_2D(object):
     #chyba mogloby byc w pierwszym (nx+1,ny) i analogicznie, tylko wtedy w donorcel trzeba by inaczej j dla C zapisywac (musialoby byc j-1)
     #moglby byc (nx+1,..) jesli hlf byloby Shift(0,1)
     self.C = (
-      numpy.empty((nx+hlo+1, ny+hlo), real_t),
-      numpy.empty((nx+hlo,   ny+hlo+1), real_t)
+      numpy.empty((nx+2*hlo, ny+2*hlo), real_t),
+      numpy.empty((nx+2*hlo,   ny+2*hlo), real_t)
     )
 
   # accessor methods
   def state(self):
     return self.psi[self.n][self.i, self.j]
   def Cx(self):
-    return self.C[0]
+    return self.C[0][self.i, self.j]
   def Cy(self):
-    return self.C[1]
+    return self.C[1][self.i, self.j]
 
   # integration logic
   def solve(self, nt):
@@ -67,6 +67,11 @@ class Solver_2D(object):
         print "time, step", t, s
         self.bcx.fill_halos(self.psi[self.n])
         self.bcy.fill_halos(self.psi[self.n])
+        self.bcx.fill_halos(self.C[0])
+        self.bcy.fill_halos(self.C[0])
+        self.bcx.fill_halos(self.C[1])
+        self.bcy.fill_halos(self.C[1])
+        #pdb.set_trace()
         self.adv.op_2D(self.psi, self.n, 
           self.C, self.i, self.j, s
         )
@@ -127,6 +132,39 @@ def donorcell_2D(psi, n, C, i, j):
 def frac(nom, den):
   return numpy.where(den>0, nom/den, 0)
 
+#listing12
+def a_op(d, psi, i, j):
+  return frac(
+    psi[pi(d, i+one, j)] - psi[pi(d, i, j)],
+    psi[pi(d, i+one, j)] + psi[pi(d, i, j)]
+  )
+#listing13
+def b_op(d, psi, i, j):
+  return frac( 
+      psi[pi(d, i+one, j+one)] 
+    + psi[pi(d, i,     j+one)] 
+    - psi[pi(d, i+one, j-one)] 
+    - psi[pi(d, i,     j-one)],
+      psi[pi(d, i+one, j+one)] 
+    + psi[pi(d, i,     j+one)] 
+    + psi[pi(d, i+one, j-one)] 
+    + psi[pi(d, i,     j-one)]
+  ) / 2
+#listing14
+def antidiff_2D(d, psi, i, j, C):
+  return (
+    abs(C[d][pi(d, i+hlf, j)]) 
+    * (1 - abs(C[d][pi(d, i+hlf, j)])) 
+    * a_op(d, psi, i, j)
+    - C[d][pi(d, i+hlf, j)] 
+    * ( # zrobic f-cje vmean
+      C[d-1][pi(d, i+one, j+hlf)] + 
+      C[d-1][pi(d, i,     j+hlf)] +
+      C[d-1][pi(d, i+one, j-hlf)] + 
+      C[d-1][pi(d, i,     j-hlf)] 
+    ) / 4
+    * b_op(d, psi, i, j)
+  )
 
 
 #listing15
@@ -135,10 +173,43 @@ class Mpdata(object):
     self.n_steps = n_iters
     self.n_halos = 1
     hlo = self.n_halos
- 
+    #definiuje tak samo jak C
+    self.tmp0 = (
+      numpy.empty((nx+2*hlo, ny+2*hlo), real_t),
+      numpy.empty((nx+2*hlo,  ny+2*hlo), real_t)
+    )
+    if n_iters > 2:
+      self.tmp1 = (
+        numpy.empty(( nx+2*hlo, ny+2*hlo), real_t),
+        numpy.empty(( nx+2*hlo,   ny+2*hlo), real_t)
+      )
   def op_2D(self, psi, n, C, i, j, step):
     if step == 0:
       donorcell_2D(psi, n, C, i, j)
     else:
-      print "ZLA STEP"
+      if step == 1:
+        C_unco, C_corr = C, self.tmp0
+      elif step % 2:
+        C_unco, C_corr = self.tmp1, self.tmp0
+      else:
+        C_unco, C_corr = self.tmp0, self.tmp1
 
+      im = i - one # przeniesc do konstruktora
+      jm = j - one
+
+      
+      C_corr[0][im+hlf, j] = (
+        antidiff_2D(0, psi[n], im, j, C_unco)) 
+      C_corr[1][i, jm+hlf] = (
+        antidiff_2D(1, psi[n], jm, i, C_unco)) 
+
+      #pdb.set_trace()
+      bcx =  Cyclic(0, i, self.n_halos)
+      bcy =  Cyclic(1, j, self.n_halos)
+      bcx.fill_halos(C_corr[0])
+      bcy.fill_halos(C_corr[0])
+      bcx.fill_halos(C_corr[1])
+      bcy.fill_halos(C_corr[1])
+      #pdb.set_trace()
+      
+      donorcell_2D(psi, n, C_corr, i, j)
--- paper/pyt/testing_numpy_zmianahalo.py
+++ paper/pyt/testing_numpy_zmianahalo.py
@@ -2,6 +2,7 @@
 from listings_zmianahalo import *
 import numpy
 import numpy.testing
+import pdb
 
 knownValue = [[3,3,1,0.5,0.5,
                numpy.array([[0., 0, 0],
@@ -42,14 +43,46 @@ knownValue = [[3,3,1,0.5,0.5,
                             [0,   0., 0]]),
                numpy.array([[0, 0.,  0],
                             [0, 0., 1.],
-                            [0, 0., 0]])]
-                ]
+                            [0, 0., 0]])],
+              #[3,3,1,0.2,0.2,
+              # numpy.array([[0., 0, 0],
+              #              [0,  1, 0],
+              #              [0,  0, 0]]),
+              # numpy.array([[0, 0,   0],
+              #              [0, 0.6, 0.2],
+              #              [0, 0.2, 0.]]),
+              # numpy.array([[0, 0,     0],
+              #              [0, 0.64,  0.18],
+              #              [0, 0.18,  0]])],
+              #[3,3,1,0.1,0.5,
+              # numpy.array([[0., 0, 0],
+              #              [0,  1, 0],
+              #              [0,  0, 0]]),
+              # numpy.array([[0, 0,   0],
+              #              [0, 0.4, 0.5],
+              #              [0, 0.1, 0]]),
+              # numpy.array([[0, 0,       0],
+              #              [0, 0.4068,  0.5011],
+              #              [0, 0.0921,  0]])],
+              [3,3,1,0.1,0.5,
+               numpy.array([[0., 0, 0],
+                            [0,  0, 0],
+                            [0,  0, 1]]),
+               numpy.array([[0,   0,   0.1],
+                            [0,   0.,  0.],
+                            [0.5, 0.,  0.4]]),
+               numpy.array([[0,      0.,      0.0921],
+                            [0,      0.,     0.],
+                            [0.5011, 0.,     0.4068]])]
+              ]
 
 def testMpdata(nx, ny, nt, cx, cy, n_iters, psi_in, psi_out):
     slv = Solver_2D(Mpdata, n_iters, Cyclic, Cyclic, nx, ny)
     slv.state()[:] = psi_in
+    #pdb.set_trace()
     slv.Cx()[:] = cx
     slv.Cy()[:] = cy
+    #pdb.set_trace()
     print "ind", slice(slv.i.start - 1, slv.i.stop),slv.j
     adwekcja = slv.adv
     print "slv.adv", adwekcja.n_halos
@@ -64,7 +97,7 @@ def test_aop(psi_in):
     pass
 def main():
     for nx, ny, nt, cx, cy, psi_in, psi_out1, psi_out2 in knownValue:
-        testMpdata(nx, ny, nt, cx, cy, 1, psi_in, psi_out1)
-#        testMpdata(nx, ny, nt, cx, cy, 2, psi_in, psi_out2)
+        #testMpdata(nx, ny, nt, cx, cy, 1, psi_in, psi_out1)
+        testMpdata(nx, ny, nt, cx, cy, 3, psi_in, psi_out2)
        
 main()
