f62ce9aa14f5e6977e9eee0be614c35b9caf0cfa
Sylwester Arabas <sarabas@igf.fuw.edu.pl>
2012-11-09 18:28:40 -0700

    making the test.py PyPy-compliant (nummpy.loadtxt -> numpy.fromstring; numpy.testing -> numpy.any)

--- paper/pyt/test.py
+++ paper/pyt/test.py
@@ -1,7 +1,12 @@
 from listings import *
 import sys
-from numpy import loadtxt
-from numpy.testing import assert_almost_equal
+
+def read_file(fname, nx, ny):
+  tmp = numpy.empty((nx, ny), real_t)
+  with open(fname, 'r') as f:
+    for x in range(0, nx):
+      tmp[x,:] = numpy.fromstring(f.readline(), dtype=real_t, sep='\t')
+  return tmp
 
 if (len(sys.argv) != (9 + 1)) : 
   raise Exception('expecting 9 arguments (nx, ny, Cx, Cy, nt, it, f.in, f.out, dec)')
@@ -17,9 +22,10 @@ fout = sys.argv[8]
 dec = int(sys.argv[9])
 
 slv = Solver_2D(Mpdata, it, Cyclic, Cyclic, nx, ny)
-slv.state()[:] = loadtxt(fin)
+slv.state()[:] = read_file(fin, nx, ny)
 slv.Cx()[:] = Cx
 slv.Cy()[:] = Cy
 slv.solve(nt)
 
-assert_almost_equal(slv.state(), loadtxt(fout), dec)
+if (abs(slv.state() - read_file(fout, nx, ny)) >= .5 * pow(10, -dec)).any(): 
+  raise Exception()
