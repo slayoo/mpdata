72d80c2ebfee41957d0b4589a4ba428a9055f01e
slayoo <slayoo@igf.fuw.edu.pl>
2012-12-26 20:24:14 +0100

    fixing test_arrvec.f

--- paper/for/test_arrvec.f
+++ paper/for/test_arrvec.f
@@ -1,10 +1,17 @@
 program test_arrvec
   use arrvec_m
   class(arrvec_t), pointer :: psi
+  integer, pointer, contiguous :: i(:), j(:)
+  integer :: c, nx = 10
+
+  allocate(i(nx), j(nx))
+  i = (/ (c, c=0, nx-1) /)
+  j = (/ (c, c=-2, nx-3) /)
 
   allocate(psi)
   call psi%ctor(2)
-  !call psi%init(0, 0, 3, 0, 4)
+  call psi%init(0, i, j)
+  call psi%init(1, i, j)
 
   print*, psi%at(0)%p%a
   print*, psi%at(0)%p%a(1,1)
@@ -14,5 +21,6 @@ program test_arrvec
 
   call psi%dtor
   deallocate(psi)
+  deallocate(i, j)
 end
 
