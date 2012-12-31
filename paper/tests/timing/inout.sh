000a313a5435ce527bf7c59badb6efec9d528446
slayoo <slayoo@igf.fuw.edu.pl>
2012-12-31 10:05:43 +0100

    new timing test settings and time output logging incl. memory usage

--- paper/tests/timing/inout.sh
+++ paper/tests/timing/inout.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 Cx=.2
 Cy=.5
-it=3
-nt=100
+nt=10
 
-for nxy in {0032,0064,0128,0256,0512,1024}; do
-  ../../cpp/inout-cpp $nxy $nxy $Cx $Cy $nt $it \
-    1>"nx=${nxy}_ny=${nxy}_Cx=${Cx}_Cy=${Cy}_nt=${nt}_it=${it}:in" \
-    2>"nx=${nxy}_ny=${nxy}_Cx=${Cx}_Cy=${Cy}_nt=${nt}_it=${it}:out"
+for it in {1,2,3}; do
+  for nxy in {0032,0064,0128,0256,0512,1024,2048}; do
+    ../../cpp/inout-cpp $nxy $nxy $Cx $Cy $nt $it \
+      1>"nx=${nxy}_ny=${nxy}_Cx=${Cx}_Cy=${Cy}_nt=${nt}_it=${it}:in" \
+      2>"nx=${nxy}_ny=${nxy}_Cx=${Cx}_Cy=${Cy}_nt=${nt}_it=${it}:out"
+  done
 done
