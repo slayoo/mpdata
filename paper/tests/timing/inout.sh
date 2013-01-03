565ea8ab31ab9331d98b1bd78303b80a81ff7f1e
slayoo <sarabas@igf.fuw.edu.pl>
2013-01-04 00:41:02 +0100

    inout.sh: some more verbose info on what is happening

--- paper/tests/timing/inout.sh
+++ paper/tests/timing/inout.sh
@@ -6,8 +6,8 @@ it=3
 #for it in {1,2,3}; do
   for nxy in {0064,0081,0102,0128,0161,0203,0256,0323,0406,0512,0645,0813,1024,1290,1626,2048,2580,3251,4096}; do
     nt=`echo "2^16/$nxy"|bc`
-echo $nt
-    ../../cpp/inout-cpp $nxy $nxy $Cx $Cy $nt $it \
+    echo nx=ny=$nxy nt=$nt
+    time ../../cpp/inout-cpp $nxy $nxy $Cx $Cy $nt $it \
       1>"nx=${nxy}_ny=${nxy}_Cx=${Cx}_Cy=${Cy}_nt=${nt}_it=${it}:in" \
       2>"nx=${nxy}_ny=${nxy}_Cx=${Cx}_Cy=${Cy}_nt=${nt}_it=${it}:out"
   done
