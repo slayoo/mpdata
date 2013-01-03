bb00cabe8b692f841d3a735be43da43c6a16fc8e
slayoo <sarabas@igf.fuw.edu.pl>
2013-01-03 22:23:35 +0100

    inout.sh: new test set-ups

--- paper/tests/timing/inout.sh
+++ paper/tests/timing/inout.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 Cx=.2
 Cy=.5
-nt=64
 it=3
 
 #for it in {1,2,3}; do
-  for nxy in {0064,0096,0128,0192,0256,0384,0512,0768,1024,1536,2048,3072,4096}; do
+  for nxy in {0064,0081,0102,0128,0161,0203,0256,0323,0406,0512,0645,0813,1024,1290,1626,2048,2580,3251,4096}; do
+    nt=`echo "2^16/$nxy"|bc`
+echo $nt
     ../../cpp/inout-cpp $nxy $nxy $Cx $Cy $nt $it \
       1>"nx=${nxy}_ny=${nxy}_Cx=${Cx}_Cy=${Cy}_nt=${nt}_it=${it}:in" \
       2>"nx=${nxy}_ny=${nxy}_Cx=${Cx}_Cy=${Cy}_nt=${nt}_it=${it}:out"
