9707b053a74b62951d9305df69a9e10fe6590337
slayoo <slayoo@igf.fuw.edu.pl>
2013-01-21 09:09:58 +0100

    adding info on mean number of instruction per cycle to the plots

--- paper/tests/timing/filter.sh
+++ paper/tests/timing/filter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-egrep "^time:| of all " Testing/Temporary/LastTest.log \
+egrep "^time:| of all | insns per cycle " Testing/Temporary/LastTest.log.tmp \
   | awk -f filter1.awk | tr -s " " | tr -s "%" | tr "%" "0" \
   | awk -f filter2.awk \
   | awk -f filter3.awk 
