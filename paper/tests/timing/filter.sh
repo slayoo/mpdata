2db6a9c2211e8b1c08d40f644847c2e145ede398
slayoo <slayoo@igf.fuw.edu.pl>
2013-01-21 16:37:40 +0100

    plot.gpi cosmetics

--- paper/tests/timing/filter.sh
+++ paper/tests/timing/filter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-egrep "^time:| of all | insns per cycle " Testing/Temporary/LastTest.log.tmp \
+egrep "^time:| of all | insns per cycle " Testing/Temporary/LastTest.log \
   | awk -f filter1.awk | tr -s " " | tr -s "%" | tr "%" "0" \
   | awk -f filter2.awk \
   | awk -f filter3.awk 
