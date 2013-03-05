2988b0cac3d27424ead752f54d18941b72da55da
Dorota Jarecka <djarecka@gmail.com>
2013-03-05 13:17:29 -0700

    adding import numba and autojit for almost all functions, pi was changed (numba didn't like f(*arg)); autojit doesn't work frac(numba: Comparing objects is not yet supported)

--- paper/pyt/numba/listings_noslice.py
+++ paper/pyt/numba/listings_noslice.py
@@ -15,20 +15,46 @@ try:
   numpy.seterr(all='ignore')
 except AttributeError:
   pass
+
+try:
+    from numba import jit, float64, int_, void, autojit
+    float64_1d=float64[:]
+    float64_2d=float64[:,:]
+    print "Numba or Numbapro imported"
+except ImportError:
+    def void(*args):
+        def dont_decorate(fn):
+            return fn
+        return dont_decorate
+    float64 = void
+    float64_1d = void
+    float64_2d = void
+    int_ = void
+    def jit(cl):
+        return cl
+    autojit = jit
+    print "Not using Numba nor Numbapro"
+
 import pdb
+
 #listing03
+@autojit
 def sl_add(sl, int_num):
   return slice(sl.start + int_num, sl.stop + int_num)
 
 #listing05
+@autojit
 def ext(r, lft, rgh):
   return slice(
     r.start - lft, 
     r.stop + rgh
   )
 #listing06
-def pi(d, *idx): 
+@autojit
+def pi(d, idx_0, idx_1):
+  idx = [idx_0, idx_1]
   return (idx[d], idx[d-1]) 
+
 #listing07
 class Solver(object):
   # ctor-like method
@@ -111,13 +137,16 @@ class Cyclic(object):
     )
 
 #listing09
+@autojit
 def f(psi_l, psi_r, C):
   #pdb.set_trace()
   return (
     (C + abs(C)) * psi_l + 
     (C - abs(C)) * psi_r
   ) / 2
+
 #listing10
+@autojit
 def donorcell(d, psi, C, i, j):
   #pdb.set_trace()
   return (
@@ -132,13 +161,16 @@ def donorcell(d, psi, C, i, j):
         C[pi(d, sl_add(i,-1), j)]
     ) 
   )
+
 #listing11
+@autojit
 def donorcell_op(psi, n, C, i, j):
   #pdb.set_trace()
   psi[n+1][i,j] = (psi[n][i,j] 
     - donorcell(0, psi[n], C[0], i, j)
     - donorcell(1, psi[n], C[1], j, i)
   )
+
 #listing12
 class Donorcell(Solver):
   def __init__(self, bcx, bcy, nx, ny):
@@ -149,17 +181,23 @@ class Donorcell(Solver):
       self.psi, self.n, 
       self.C, self.i.start, self.i.stop, self.j.start, self.j.stop
     )
+
 #listing13
+#@autojit
 def frac(nom, den):
   return numpy.where(den > 0, nom/den, 0)
+
 #listing14
+@autojit
 def a_op(d, psi, i, j):
   #pdb.set_trace()
   return frac(
     psi[pi(d, sl_add(i,1), j)] - psi[pi(d, i, j)],
     psi[pi(d, sl_add(i,1), j)] + psi[pi(d, i, j)]
   )
+
 #listing15
+@autojit
 def b_op(d, psi, i, j):
   return frac( 
     psi[pi(d, sl_add(i,1), sl_add(j,1))] + psi[pi(d, i, sl_add(j,1))] -
@@ -167,13 +205,17 @@ def b_op(d, psi, i, j):
     psi[pi(d, sl_add(i,1), sl_add(j,1))] + psi[pi(d, i, sl_add(j,1))] +
     psi[pi(d, sl_add(i,1), sl_add(j,-1))] + psi[pi(d, i, sl_add(j,-1))]
   ) / 2
+
 #listing16
+@autojit
 def C_bar(d, C, i, j):
   return (
     C[pi(d, sl_add(i,1), sl_add(j,0))] + C[pi(d, i,  sl_add(j,0))] +
     C[pi(d, sl_add(i,1), sl_add(j,-1))] + C[pi(d, i,  sl_add(j,-1))] 
   ) / 4
+
 #listing17
+@autojit
 def antidiff(d, psi, i, j, C):
   return (
     abs(C[d][pi(d, sl_add(i,0), j)]) 
